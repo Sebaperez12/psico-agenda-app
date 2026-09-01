6d497f3eabbaae0b6a41b33ddf668f3f025218ff
Sebastian Perez <sebaperez12@gmail.com>
2026-09-01 18:30:39 +0200

    Use password eye image assets

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import logo from "../assets/logo 6.png";
+import eyeClosedIcon from "../assets/ojo cerrado.png";
+import eyeOpenIcon from "../assets/ojo abierto.png";
 import portada from "../assets/PORTADA.png";
 import api from "../services/api";
 import "./Login.css";
@@ -187,7 +189,7 @@ export default function Login() {
                 aria-label={showPassword ? "Ocultar contrasena" : "Mostrar contrasena"}
                 title={showPassword ? "Ocultar contrasena" : "Mostrar contrasena"}
               >
-                <span aria-hidden="true" />
+                <img src={showPassword ? eyeOpenIcon : eyeClosedIcon} alt="" aria-hidden="true" />
               </button>
             </div>
             {!isRegister && (
