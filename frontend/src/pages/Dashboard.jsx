6f53f2cb0221f4ec2d58ef4bd115bc42a70b67b7
Sebastian Perez <sebaperez12@gmail.com>
2026-09-11 14:19:57 +0200

    Use lighter dashboard header image

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import dashboardImage from "../assets/dashboard.png";
+import dashboardImage from "../assets/dashboard-web.jpg";
 import api from "../services/api";
 import "./Dashboard.css";
 
